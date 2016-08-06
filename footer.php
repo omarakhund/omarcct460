d93494b73a84ae15f1dff229202bad919a29a62b
omarakhund <mak@Omars-MacBook-Pro.local>
2016-08-06 16:26:01 -0400

    Working on styles

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package omar460
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'omar460' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'omar460' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'omar460' ), 'omar460', '<a href="http://www.twitter.com/omarzaffar" rel="designer">Omar Akhund</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
