c47e0bbaa550255c1accfe9cdfe00a6b85ee782c
omarakhund <mak@utmuser54-141.wireless.utoronto.ca>
2016-08-11 15:38:49 -0400

    Displaying test CPT

--- functions.php
+++ functions.php
@@ -37,6 +37,15 @@ register_post_type('movies' $args);
 
 add_action('init','custom_post', 0);
 
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'movies' ) );
+    return $query;
+}
+
+
 
 
 
