893b0b8142997e6b396bf8965077da6a04b3801e
omarakhund <mak@utmuser54-141.wireless.utoronto.ca>
2016-08-11 16:28:34 -0400

    pdf custom post type
    Post will be displayed on the menu tab

--- functions.php
+++ functions.php
@@ -15,39 +15,79 @@
 
 //custom post type code begins. Code reference: http://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/
 
-function custom_post(){
-	$labels = array(
-		'name'                => _x( 'Movies', 'Post Type General Name', 'twentythirteen' ),
-        'singular_name'       => _x( 'Movie', 'Post Type Singular Name', 'twentythirteen' ),
-        'menu_name'           => __( 'Movies', 'twentythirteen' ),
-        'parent_item_colon'   => __( 'Parent Movie', 'twentythirteen' ),
-        'all_items'           => __( 'All Movies', 'twentythirteen' ),
-        'view_item'           => __( 'View Movie', 'twentythirteen' ),
-        'add_new_item'        => __( 'Add New Movie', 'twentythirteen' ),
-        'add_new'             => __( 'Add New', 'twentythirteen' ),
-        'edit_item'           => __( 'Edit Movie', 'twentythirteen' ),
-        'update_item'         => __( 'Update Movie', 'twentythirteen' ),
-        'search_items'        => __( 'Search Movie', 'twentythirteen' ),
-        'not_found'           => __( 'Not Found', 'twentythirteen' ),
-        'not_found_in_trash'  => __( 'Not found in Trash', 'twentythirteen' ),
+
+function omar_cpt() {
+    register_post_type( 'pdf',
+    // CPT Option
+        array(
+            'labels' => array(
+                'name' => __( 'Pakistan Development Fund' ),
+                'singular_name' => __( 'PDF' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'pdf'),
+        )
     );
 }
 
-register_post_type('movies' $args);
+// Hooking up our function to theme setup
+add_action( 'init', 'omar_cpt' );
 
-add_action('init','custom_post', 0);
+function omar_custom() {
+// Set UI labels for Custom Post Type
+    $labels = array(
+        'name'                => _x( 'PDF', 'Post Type General Name', 'twentythirteen' ),
+        'singular_name'       => _x( 'PDFs', 'Post Type Singular Name', 'twentythirteen' ),
+        'menu_name'           => __( 'Pakistan Development Fund', 'twentythirteen' ),
+        'parent_item_colon'   => __( 'Parent pdf', 'twentythirteen' ),
+        'all_items'           => __( 'All projects', 'twentythirteen' ),
+        'view_item'           => __( 'View project', 'twentythirteen' ),
+        'add_new_item'        => __( 'Add new project', 'twentythirteen' ),
+        'add_new'             => __( 'Add New', 'twentythirteen' ),
+        'edit_item'           => __( 'Edit Project', 'twentythirteen' ),
+        'update_item'         => __( 'Update Project', 'twentythirteen' ),
+        'search_items'        => __( 'Search Project', 'twentythirteen' ),
+        'not_found'           => __( 'Not Found', 'twentythirteen' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'twentythirteen' ),
+    );
 
-add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+// Set other options for Custom Post Type
+    $args = array(
+        'label'               => __( 'projects', 'twentythirteen' ),
+        'description'         => __( 'pdf news and media', 'twentythirteen' ),
+        'labels'              => $labels,
+        // Features this CPT supports in Post Editor
+        'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+        // You can associate this CPT with a taxonomy or custom taxonomy.
+        'taxonomies'          => array( 'genres' ),
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'can_export'          => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'page',
+    );
 
-function add_my_post_types_to_query( $query ) {
-    if ( is_home() && $query->is_main_query() )
-        $query->set( 'post_type', array( 'post', 'movies' ) );
-    return $query;
+    register_post_type( 'pdf', $args );
+
 }
 
+add_action( 'init', 'omar_custom', 0 );
 
 
-
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'pdf' ) );
+    return $query;
+}
 
 //Footer menu
 register_nav_menus(
